c2dbec5e5e79437ac7c230890e820747344b910c
Alejo Castaño <alejo@experimentality.co>
2022-01-14 12:57:59 -0500

    Changed to some implementation

--- src/BusinessLogic/invoice-process.ts
+++ src/BusinessLogic/invoice-process.ts
@@ -2,9 +2,5 @@ import { Order } from "../Interfaces/Order";
 
 export function generateInvoiceFromOrder(order: Order) {
     const productsPrice = order.products.map(product => product.quantity > 10 ? -1 : product.quantity * product.unitPrice);
-    if (productsPrice.indexOf(-1) > -1) {
-        return false
-    } else {
-        return true
-    }
+    return !productsPrice.some(price => price == -1)
 }
